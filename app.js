123689941ef594551a410cb3036760dac3821458
Maria Belen Legresti <mariabelenlegresti@gmail.com>
2021-09-28 19:48:10 -0300

    configuracion de puerto

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const publicPath = path.join(__dirname, "./public");
 app.use(express.static(publicPath)); /*le digo a express que quiero tener la carpeta "public" como un 
                                        recurso de archivos estáticos; */
 
-app.listen(3000, () => {
+app.listen(process.env.PORT || 3000, () => {
     console.log("Servidor corriendo en http://localhost:3000");
 });
 
